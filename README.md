899e6742ca5bfa66d90875c414a020ec67d519d8
Nathan Betts <nathanbetts@hotmail.co.uk>
2016-10-24 21:44:22 +0100

    add help messages for problems compiling OpenGL on MacOS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fractals
+To compile: ./compile_opengl.sh fractals.cpp
+Ensure you have Xcode and Xquartz installed to compile with Glut on Mac
--- compile_opengl.sh
+++ compile_opengl.sh
@@ -5,7 +5,9 @@ program=${file%.*}
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   g++ -std=c++11 -Wall -Wno-deprecated -O3 -fomit-frame-pointer -pipe -DFX -DXMESA -lGL -lGLU -lglut -lX11 -lm -g $file -o $program
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Ensure you have Xcode and Xquartz installed to compile with Glut on Mac
   g++ -std=c++11 -Wall -Wno-deprecated -O3 -framework GLUT -framework OpenGL -I/usr/X11/include -L/usr/X11/lib -lX11 $file -o $program
+  # g++ -std=c++11 -Wall -Wno-deprecated -O3 -framework GLUT -framework OpenGL -I/usr/X11R6/include -L/usr/X11R6/lib -lX11 $file -o $program
 else
   echo "OS not supported"
 fi
